01483cb6594c774f94f143dd7905085b8c08559a
felipegoraroficial <felipegoraroficial>
2025-01-07 19:59:34 +0000

    modificando função para tratativas de valores monetarios

--- convert_currency_column.py
+++ convert_currency_column.py
@@ -1,3 +1,5 @@
+from pyspark.sql.functions import col, regexp_replace
+
 def convert_currency_column(df, col_name):
     """
     Converte uma coluna de moeda no DataFrame para o tipo double.
@@ -9,7 +11,8 @@ def convert_currency_column(df, col_name):
     Retorna:
     DataFrame: O DataFrame com a coluna de moeda convertida para double.
     """
-    df = df.withColumn(col_name, regexp_replace(col(col_name), "R\\$\\s", ""))
+    df = df.withColumn(col_name, regexp_replace(col(col_name), "[^0-9,R\\$]", ""))
+    df = df.withColumn(col_name, regexp_replace(col(col_name), "R\\$", ""))
     df = df.withColumn(col_name, regexp_replace(col(col_name), "\\.", ""))
     df = df.withColumn(col_name, regexp_replace(col(col_name), ",", "."))
     df = df.withColumn(col_name, col(col_name).cast("double"))
